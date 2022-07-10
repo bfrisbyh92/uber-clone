1d72c19bbd312fa6f50b9448e0ca1455b6edfc73
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-07-10 17:31:45 -0400

    Redux setOrigin working. Fixed a bug in navSlice that was causing error with Redux. Reducer on navSlice needed to be renamed to reducers plural

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,14 +2,23 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions'
-const uberLogo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Caa15xOsNCSaVucg7kPZck9cSzqhfJShug&usqp=CAU'
 import { useNavigation } from '@react-navigation/native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_API_KEY } from "@env";
 
+// Redux
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
+
+const uberLogo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Caa15xOsNCSaVucg7kPZck9cSzqhfJShug&usqp=CAU'
+// ^^^ Need to change to change to image saved inside assets directory before deployment 
+
 const HomeScreen = () => {
 
+const dispatch = useDispatch();
+
 const navigation = useNavigation(navigation);
+// useNavigation hook instead of passing a destructured 'navigation' to 'HomeScreen'
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
@@ -27,6 +36,7 @@ const navigation = useNavigation(navigation);
              />
 
               <GooglePlacesAutocomplete
+              // ^^^ Bug. This only shows up on mobile I will try to fix that later but for now only mobile is okay
                 styles={{
                   container: {
                     flex: 0,
@@ -40,17 +50,28 @@ const navigation = useNavigation(navigation);
                   key: GOOGLE_API_KEY,
                   language: 'en',
                 }}
+
+                onPress={(data, details = null ) => {
+                  console.log(data, setOrigin);
+                  console.log(details);
+                  console.log(setOrigin);
+
+                  dispatch(setOrigin({
+                    location: details.geometry.location,
+                    description: details.description
+                  }))
+                  dispatch(setDestination(null))
+                }}
+                // ^^^ Storing origin in Redux to be used later when calculating ride
                 enablePoweredByContainer={false}
                 minLength={2}
                 placeholder='Where From?'
                 nearbyPlacesAPI="GooglePlacesSearch"
                 debounce={900}
                 // ^^^ Change debounce to 400 later. No reason to call the API a million times during production as long as it's working
-                onPress={(data, details = null ) => {
-                  console.log(data);
-                  console.log(details);
-                }}
+                
                 fetchDetails={true}
+                returnKeyType={"search"}
               />
 
              <NavOptions />
