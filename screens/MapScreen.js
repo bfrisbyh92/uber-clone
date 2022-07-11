0b9953e64d6f61386347cbed2cfb6e1f9428840b
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-07-10 21:29:43 -0400

    Added new stack on the MapScreen and added googles autocomplete to the input field for the destination and sending that info to redux

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,12 +3,15 @@ import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
-// import MapView from 'react-native-maps';
-
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
+import { createStackNavigator } from '@react-navigation/stack';
+// Needs it's own navigation for each option you'd select similiar to App.js's Stack Navigation
 
 
 const MapScreen = () => {
 
+  const Stack = createStackNavigator();
   const navigation = useNavigation();
 
   return (
@@ -19,7 +22,25 @@ const MapScreen = () => {
     </View>
 
     <View style={tw`h-1/2`}>
+      <Stack.Navigator>
 
+        <Stack.Screen
+          name="NavigateCard"
+          component={ NavigateCard }
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="RideOptionsCard"
+          component={ RideOptionsCard }
+          options={{
+            headerShown: false,
+          }}
+        />
+        
+       </Stack.Navigator>
     </View>
 
     </View>
